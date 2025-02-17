f9273c7d1e18fc48eb5eb0daffc982ac2d5b5577
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2025-02-17 18:58:38 +0530

    added login user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.models.js';
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export async function getusers(req, res) {
   try {
@@ -26,6 +27,23 @@ export async function createUser(req, res) {
     });
     await user.save();
     return res.status(201).json({ message: "User created successfully", user });
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function loginuser(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send("User not found");
+    }
+    const validpassword = bcrypt.compareSync(password, user.password);
+    if (!validpassword) {
+      return res.status(400).send("Invalid Password");
+    }
 
   } catch (error) {
     console.log(error);
